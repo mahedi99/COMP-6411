2bdbb245c29fc4991b77c7abe7c3ca357e759d75
mahedi99 <mahedi9000@gmail.com>
2018-05-16 04:00:18 -0400

    done!

--- Read File/main.py
+++ Read File/main.py
@@ -1,105 +1,115 @@
-student_arr = []
-first_name = []
-last_name = []
-a1 = []
-a2 = []
-project = []
-test1 = []
-test2 = []
+from operator import attrgetter
+from model import Model
+
 a1_grade_scale = 0
 a2_grade_scale = 0
 project_grade_scale = 0
 test1_grade_scale = 0
 test2_grade_scale = 0
-total_marks = []
-grade = []
+pass_fail = 50
+
+models = []
 
 
 def option_1():
     fmt = '{:<8}{:20}{:<20}'
-    print("Enter A1,A2,PR,T1,T2 : ")
+    print("Enter A1/A2/PR/T1/T2 : ")
 
     while True:
         individual_comp = input()
         if individual_comp == "A1":
             print("A1 grades (%.1f)" % a1_grade_scale)
-            for i in range(len(student_arr)):
-                full_name = first_name[i] + "," + last_name[i]
-                print(fmt.format(student_arr[i], full_name, a1[i]))
+            for i in range(len(models)):
+                full_name = models[i].first_name + "," + models[i].last_name
+                print(fmt.format(models[i].student_id, full_name, models[i].a1))
             break
         elif individual_comp == "A2":
             print("A2 grades (%.1f)" % a2_grade_scale)
-            for i in range(len(student_arr)):
-                full_name = first_name[i] + "," + last_name[i]
-                print(fmt.format(student_arr[i], full_name, a2[i]))
+            for i in range(len(models)):
+                full_name = models[i].first_name + "," + models[i].last_name
+                print(fmt.format(models[i].student_id, full_name, models[i].a2))
             break
         elif individual_comp == "PR":
             print("Project grades (%.1f)" % project_grade_scale)
-            for i in range(len(student_arr)):
-                full_name = first_name[i] + "," + last_name[i]
-                print(fmt.format(student_arr[i], full_name, project[i]))
+            for i in range(len(models)):
+                full_name = models[i].first_name + "," + models[i].last_name
+                print(fmt.format(models[i].student_id, full_name, models[i].project))
             break
         elif individual_comp == "T1":
             print("A1 grades (%.1f)" % test1_grade_scale)
-            for i in range(len(student_arr)):
-                full_name = first_name[i] + "," + last_name[i]
-                print(fmt.format(student_arr[i], full_name, test1[i]))
+            for i in range(len(models)):
+                full_name = models[i].first_name + "," + models[i].last_name
+                print(fmt.format(models[i].student_id, full_name, models[i].t1))
             break
         elif individual_comp == "T2":
             print("A1 grades (%.1f)" % test2_grade_scale)
-            for i in range(len(student_arr)):
-                full_name = first_name[i] + "," + last_name[i]
-                print(fmt.format(student_arr[i], full_name, test2[i]))
+            for i in range(len(models)):
+                full_name = models[i].first_name + "," + models[i].last_name
+                print(fmt.format(models[i].student_id, full_name, models[i].t2))
             break
         else:
             print("Wrong input type, please follow the instructions above.")
 
 
 def option_2():
-    print("Enter A1,A2,PR,T1,T2 : ")
+    print("Enter A1/A2/PR/T1/T2 : ")
 
     while True:
         individual_comp = input()
         total = 0
         if individual_comp == "A1":
-            for x in a1:
-                total += x
-            total = total/len(a1)
+            for x in models:
+                total += x.a1
+            total = total/len(models)
             print("%.1f/%.1f" % (total, a1_grade_scale))
         elif individual_comp == "A2":
-            for x in a2:
-                total += x
-            total = total/len(a2)
+            for x in models:
+                total += x.a2
+            total = total/len(models)
             print("%.1f/%.1f" % (total, a2_grade_scale))
         elif individual_comp == "PR":
-            for x in project:
-                total += x
-            total = total/len(project)
+            for x in models:
+                total += x.project
+            total = total/len(models)
             print("%d/%d" % (total, project_grade_scale))
         elif individual_comp == "T1":
-            for x in test2:
-                total += x
-            total = total/len(test1)
+            for x in models:
+                total += x.t1
+            total = total/len(models)
             print("%d/%d" % (total, test1_grade_scale))
         elif individual_comp == "T2":
-            for x in test2:
-                total += x
-            total = total/len(test2)
+            for x in models:
+                total += x.t2
+            total = total/len(models)
             print("%d/%d" % (total, test2_grade_scale))
         break
 
 
-def option_3():
-    calculate_grades()
+def option_3(model_list):
     fmt = '{:<8}{:10}{:10}{:<8}{:<8}{:<8}{:<8}{:<8}{:<8}{:<8}'
     print(fmt.format('ID', 'LN', 'FN', 'A1', 'A2', 'PR', 'T1', 'T2', 'GR', 'FL'))
-    for i, (id, lnm, fnm, as1, as2, prj, tr1, tr2, gr, fl) in enumerate(
-            zip(student_arr, first_name, last_name, a1, a2, project, test1, test2, total_marks, grade)):
-        print(fmt.format(id, lnm, fnm, as1, as2, prj, tr1, tr2, gr, fl))
+    for model in model_list:
+        print(fmt.format(model.student_id, model.last_name, model.first_name, model.a1, model.a2, model.project, model.t1, model.t2, model.marks, model.grade))
 
 
 def option_4():
-    print("")
+    print("Sort by LT/GR:")
+    while True:
+        sort_option = input()
+        if sort_option == "LT":
+            option_3(sorted(models, key=attrgetter('last_name')))
+            break
+        elif sort_option == "GR":
+            option_3(sorted(models, key=attrgetter('marks'), reverse=True))
+            break
+
+
+def option_5():
+    tmp_pass_fail = int(input("Enter Pass/Fail number: "))
+    if 0 < tmp_pass_fail < 100:
+        calculate_grades(tmp_pass_fail)
+        option_3(models)
+    calculate_grades(pass_fail)
 
 
 def read_files():
@@ -109,80 +119,103 @@ def read_files():
     global test1_grade_scale
     global test2_grade_scale
 
+    global models
+
     with open(r"C:\Users\mahedi\Desktop\COMP-6411/class.txt") as class_file:
         for line in class_file:
-            student_arr.append(line[:5])
-            first_name.append(line[line.index('|')+1:line.find('|', 6, len(line))])
-            last_name.append(line[line.find('|', 6, len(line))+1:].replace("\n", ""))
+            model = Model()
+            model.student_id = line[:5]
+            model.first_name = line[line.index('|')+1:line.find('|', 6, len(line))]
+            model.last_name = line[line.find('|', 6, len(line))+1:].replace("\n", "")
+            models.append(model)
 
     with open(r"C:\Users\mahedi\Desktop\COMP-6411/a1.txt") as a1_file:
         first = True
+        i = 0
         for line in a1_file:
             if first:
                 a1_grade_scale = float(line)
                 first = False
+
             else:
-                a1.append(float(line[6:].replace("\n", "")))
+                tmp_a1 = float(line[6:].replace("\n", ""))
+                models[i].a1 = (tmp_a1 / a1_grade_scale) * 7.5
+                i += 1
 
     with open(r"C:\Users\mahedi\Desktop\COMP-6411/a2.txt") as a2_file:
         first = True
+        i = 0
         for line in a2_file:
             if first:
                 a2_grade_scale = float(line)
                 first = False
             else:
-                a2.append(float(line[6:].replace("\n", "")))
+                models[i].a2 = float(line[6:].replace("\n", ""))
+                i += 1
 
     with open(r"C:\Users\mahedi\Desktop\COMP-6411/project.txt") as project_file:
         first = True
+        i = 0
         for line in project_file:
             if first:
                 project_grade_scale = int(line)
                 first = False
             else:
-                project.append(int(line[6:].replace("\n", "")))
+                models[i].project = float(line[6:].replace("\n", ""))
+                i += 1
 
     with open(r"C:\Users\mahedi\Desktop\COMP-6411/test1.txt") as test1_file:
         first = True
+        i = 0
         for line in test1_file:
             if first:
                 test1_grade_scale = int(line)
-                # print("%d" %test1_grade_scale)
                 first = False
             else:
-                test1.append(int(line[6:].replace("\n", "")))
+                models[i].t1 = float(line[6:].replace("\n", ""))
+                i += 1
 
     with open(r"C:\Users\mahedi\Desktop\COMP-6411/test2.txt") as test2_file:
         first = True
+        i = 0
         for line in test2_file:
             if first:
                 test2_grade_scale = int(line)
                 first = False
             else:
-                test2.append(int(line[6:].replace("\n", "")))
+                models[i].t2 = float(line[6:].replace("\n", ""))
+                i += 1
 
 
-def calculate_grades():
-    for i in range(len(student_arr)):
-        total_tmp = a1[i] + a2[i] + project[i] + test1[i] + test2[i]
-        total_marks.append(total_tmp)
-        if total_tmp < 50:
-            grade.append("F")
-        elif total_tmp < 57:
-            grade.append("C")
-        elif total_tmp < 64:
-            grade.append("B-")
-        elif total_tmp < 70:
-            grade.append("B")
-        elif total_tmp < 77:
-            grade.append("B+")
-        elif total_tmp < 84:
-            grade.append("A-")
-        elif total_tmp < 90:
-            grade.append("A+")
+def calculate_grades(tmp_pass_fail):
+    mark_range = (100 - pass_fail)/7
+    if mark_range > 0:
+        for i in range(len(models)):
+            total_tmp = models[i].a1 + models[i].a2 + models[i].project + models[i].t1 + models[i].t2
+            models[i].marks = total_tmp
+            if total_tmp <= tmp_pass_fail:
+                models[i].grade = "F"
+            elif total_tmp < tmp_pass_fail+(mark_range * 1):
+                models[i].grade = "C"
+            elif total_tmp < tmp_pass_fail+(mark_range * 2):
+                models[i].grade = "B-"
+            elif total_tmp < tmp_pass_fail+(mark_range * 3):
+                models[i].grade = "B"
+            elif total_tmp < tmp_pass_fail+(mark_range * 4):
+                models[i].grade = "B+"
+            elif total_tmp < tmp_pass_fail+(mark_range * 5):
+                models[i].grade = "A-"
+            elif total_tmp < tmp_pass_fail+(mark_range * 6):
+                models[i].grade = "A-"
+            elif total_tmp <= 100:
+                models[i].grade = "A+"
 
 
 def main():
+
+    global models
+    models = sorted(models, key=attrgetter('student_id'))
+
     ans = True
     while ans:
         print("""
@@ -199,17 +232,18 @@ def main():
         elif ans == "2":
             option_2()
         elif ans == "3":
-            option_3()
+            option_3(models)
         elif ans == "4":
-            print()
+            option_4()
         elif ans == "5":
-            print()
+            option_5()
         elif ans == "6":
             ans = None
-            print()
+            print("Good Bye")
         elif ans != "":  # null input
-            print()
+            print("Wrong input, Input range: 1-6")
 
 
 read_files()
+calculate_grades(pass_fail)
 main()
---
+++ Read File/model.py
@@ -0,0 +1,11 @@
+class Model:
+    student_id = ""
+    first_name = ""
+    last_name = ""
+    a1 = 0
+    a2 = 0
+    project = 0
+    t1 = 0
+    t2 = 0
+    marks = 0
+    grade = ''
